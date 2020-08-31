c2def4c3583dd6ae69609fad568a90c131a20a7b
Mimioj101 <mimioj@gmail.com>
2020-08-31 15:46:24 -0400

    add pokemon

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,6 +1,6 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
-const POKEMONS_URL = `${BASE_URL}/pokemons`
+const POKEMONS_URL = `${BASE_URL}/pokemons/`
 
 document.addEventListener("DOMContentLoaded", function() {
 
@@ -12,61 +12,60 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(trainerData => renderTrainers(trainerData))
     }
 
-    function getPokemon(){
-        const options = {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ }),
-        }
 
-        fetch(POKEMONS_URL)
-        .then(res => res.json())
-        .then(pokemonData => renderTrainers(pokemonData))
-    }
-
-
-    function renderTrainers(trainerData, pokemonData = null){
+    function renderTrainers(trainerData){
         for(let trainer of trainerData){
             console.log(trainer.name)
             const card = document.createElement("div")
             card.className = "card"
             card.dataset.id = trainer.id
             const dataid = card.dataset.id
-
+            card.innerHTML = `
+            <p>${trainer.name}</p>
+            <button class="add-pokemon-button" data-trainer-id="${trainer.id}">Add Pokemon</button>
+            <ul class="poke-ul">
+            ${trainer.pokemons.map( pokemon => (
+                `<li>${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button></li>`
+            ))}     
+            </ul>`
             
-            renderPokemon(card, trainerData)
             main.append(card)
             
         }
     }
-    function renderPokemon(card, trainerData) {
 
-        for(let obj of trainerData){
-            debugger
-            card.innerHTML = `
-            <p>${obj.name}</p>
-            <button class="add-pokemon-button" data-trainer-id="${obj.id}">Add Pokemon</button>
-            <li>${obj.pokemons.nickname} ${obj.pokemons.species}button class="release" data-pokemon-id="${obj.pokemons.id}">Release</button></li>
-            <ul>
-            </ul>
-            `
-        }
+    function renderPokemon(pokemon) {
+        const pokeUl = document.querySelector(".poke-ul")
+        const pokeLi = document.createElement("li")
+        pokeLi.innerHTML = `
+        ${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button>`
+        pokeUl.append(pokeLi)
     }
+
 
     function submitHandler() {
         document.addEventListener("click", e => {
-            button = e.target
-            console.log(e)
+            const button = e.target
             if(button.matches(".add-pokemon-button")){
-                console.log(`Inside the Pokemon button if`)
-                // Fetch to the Faker gem on a random pokemon
-            } //else if(button.matches){}
-        })
+                const getButtonTrainer = button.parentElement.dataset.id
+                
+                const options = {
+                    method: "POST",
+                    headers: {
+                         "content-type": "application/json",
+                         "accept": "application/json"
+                    },
+                 body: JSON.stringify({ "trainer_id": getButtonTrainer })
+                }
+                
+                fetch(POKEMONS_URL, options)
+                .then(resp => resp.json())
+                .then(pokemonData => renderPokemon(pokemonData))
+            }
+        }) 
     }
 
+
 getTrainers()
-// getPokemon()
 submitHandler()
 })
